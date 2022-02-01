bf832471b198b0cccfa8fc939a182e48f86d47c4
Mohit Bagaria <mohitbagaria@Mohits-MacBook-Air.local>
2022-02-01 13:01:20 +0530

    Modified

--- Fall_detection_script.py
+++ Fall_detection_script.py
@@ -20,28 +20,18 @@ def extract_mean_max_min(data,start_time,win_size=50): #25Hz sampling rate
     
 
 def detect_free_fall(df,threshold_tending_0,impact_activity,inactivity,win_size=50,overlap_win=25):
-    
+    faller=0
     for i in range(0,df.index[-1]-win_size,overlap_win): 
+        if(faller>0):
+            faller-=1
+            continue
         avg_mag,min_mag,max_mag,diff_max_min=extract_mean_max_min(df,i,win_size)
 
         if(min_mag<=threshold_tending_0):#fall detected
             print(i,"Free Fall detected")
             #case where impact and free fall in same window
             if(diff_max_min>=impact_activity):#min max diff
-                print(i,"Impact also deteced-SAME WINDOW")
-                temp_size=10 #window size for calculating average
-                no_of_iterations = 15 #considering 2s [10*10==100 -> 25Hz in 1s]
-                sum_diff=0
-                for j in range(0,no_of_iterations+1):
-                    sum_diff+=extract_mean_max_min(df,i+win_size+ (j*temp_size),temp_size)[3]
-                avg_sum_diff = sum_diff/no_of_iterations
-                if(avg_sum_diff<=inactivity):
-                    print(i,"Inactivness, FALL CONFIRMED")
-                else:
-                    print(i,"Inactivness not detected, but value:",avg_sum_diff)   
-            #Searching for Impact in next window
-            elif(extract_mean_max_min(df,i+overlap_win,overlap_win)[3]>=impact_activity):#min max diff
-                print(i,"Impact also deteced-NEXT WINDOW")
+                print(i,"Impact detected-SAME WINDOW, val:",diff_max_min)
                 temp_size=5 #window size for calculating average
                 no_of_iterations = 30 #considering 2s [10*10==100 -> 25Hz in 1s]
                 sum_diff=0
@@ -50,10 +40,28 @@ def detect_free_fall(df,threshold_tending_0,impact_activity,inactivity,win_size=
                 avg_sum_diff = sum_diff/no_of_iterations
                 if(avg_sum_diff<=inactivity):
                     print(i,"Inactivness, FALL CONFIRMED")
+                    faller=2
+                else:
+                    print(i,"Inactivness not detected, but value:",avg_sum_diff)   
+            #Searching for Impact in next window
+            elif(extract_mean_max_min(df,i+overlap_win,overlap_win)[3]>=impact_activity):#min max diff
+                print(i,"Impact detected-NEXT WINDOW")
+                temp_size=5 #window size for calculating average
+                no_of_iterations = 30 #considering 2s [10*10==100 -> 25Hz in 1s]
+                sum_diff=0
+                for j in range(0,no_of_iterations+1):
+                    sum_diff+=extract_mean_max_min(df,i+win_size+ (j*temp_size),temp_size)[3]
+                avg_sum_diff = sum_diff/no_of_iterations
+                if(avg_sum_diff<=inactivity):
+                    print(i,"Inactivness, FALL CONFIRMED")
+                    faller=2
                 else:
                     print(i,"Inactivness not detected, but value:",avg_sum_diff)    
 
+
+#Give parent folder path too data
 folder_path = '/Users/mohitbagaria/Fall-Detection-using-Accelerometer/test_datas'
+#Ensure data os in csv format
 file_list = glob.glob(folder_path + "/*.csv")
 sys.stdout=open("/Users/mohitbagaria/Fall-Detection-using-Accelerometer/test_outputs.txt","w")
 
@@ -63,6 +71,6 @@ for i in range(0,len(file_list)):
     df=df.loc[:,1:3]
     df.columns=['X','Y','Z']
     df['Magnitude']=np.sqrt(df['X']*df['X']+df['Y']*df['Y']+df['Z']*df['Z'])
-    print(detect_free_fall(df,120,2000,200))
+    print(detect_free_fall(df,500,2000,300))
 
 sys.stdout.close()
